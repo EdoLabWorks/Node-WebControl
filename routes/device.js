50408311350a8db50750e84ffd4b9625808a759f
Ed Alegrid <e14d@outlook.com>
2016-03-27 02:04:24 -0400

    Update device.js

--- routes/device.js
+++ routes/device.js
@@ -12,7 +12,6 @@ var TCPconnect = function (data){
 	console.log("TCP client connected");
 	this.write(data);
 	});
-
 	client.on("error", function(error) {
 	console.log(error);
 	});
@@ -29,68 +28,62 @@ var result = [];
 
 /* DEVICE 1 */
 r.route("/d1")
-   .post(function (req, res, next) {
+  .post(function (req, res, next) {
 	TCPconnect("ON1");
 	res.json({ response : result });
- })
-   .get(function (req, res, next)  {
+}).get(function (req, res, next)  {
 	TCPconnect("OFF1");
 	res.json({ response : result });
 });
 
 /* DEVICE 2 */
 r.route("/d2")
-   .post(function (req, res, next) {
+  .post(function (req, res, next) {
 	TCPconnect("ON2");
 	res.json({ response : result });
-})
-   .get(function (req, res, next)  {
+}).get(function (req, res, next)  {
 	TCPconnect("OFF2");
 	res.json({ response : result });
-})
+});
 
 /* DEVICE 3 */
 r.route("/d3")
-   .post(function (req, res, next) {
+  .post(function (req, res, next) {
 	TCPconnect("ON3");
 	res.json({ response : result });
-})
-   .get(function (req, res, next)  {
+}).get(function (req, res, next)  {
 	TCPconnect("OFF3");
 	res.json({ response : result });
-})
+});
 
 /* DEVICE 4 */
 r.route("/d4")
-    .post(function (req, res, next) {
+  .post(function (req, res, next) {
 	TCPconnect("ON4");
 	res.json({ response : result });
-})
-    .get(function (req, res, next) {
+}).get(function (req, res, next) {
 	TCPconnect("OFF4");
 	res.json({ response : result });
-})
+});
 
 /* DEVICE 5 */
 r.route("/d5") 
-    .post(function (req, res, next) {
+  .post(function (req, res, next) {
 	TCPconnect("ON5");
 	res.json({ response : result });
-})
-    .get(function (req, res, next) {
+}).get(function (req, res, next) {
 	TCPconnect("OFF5");
 	res.json({ response : result });
-})
+});
 
 /* DEVICE 6 */
 r.route("/d6")
-    .post(function (req, res, next) {
+  .post(function (req, res, next) {
 	TCPconnect("ON6");
 	res.json({ response : result });
-})
-    .get(function (req, res, next) {
+}).get(function (req, res, next) {
 	TCPconnect("OFF6");
 	res.json({ response : result });
-})
+});
 
 module.exports = r;
