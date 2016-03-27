ccd449e8ef1353f8a6816a99cbcfbbc3ab42c40d
Ed Alegrid <e14d@outlook.com>
2016-03-27 11:16:12 -0400

    code preparation for one api

--- public/js/DeviceController.js
+++ public/js/DeviceController.js
@@ -1,94 +1,73 @@
 "use strict"
 var device = angular.module("MainApp", []);
-device.controller("DeviceController", ["$scope", "RESTservice", function ($scope, RESTservice) {
+device.controller("DeviceController", ["$scope", "CtrlService", function ($scope, CtrlService) {
 	var vm = {};
 	vm.state = [];
-	
-	RESTservice.getItems().then(function (response) {
- 	vm.state = response.data.items; 
-	});
-	
+
 	// device 1
 	vm.device1ON = function () {
-		RESTservice.device1ON().then(function (response) {
-		});
-    };
+		CtrlService.device1ON();
+    	};
 	vm.device1OFF = function () {
-		RESTservice.device1OFF().then(function (response) {
-		});
-    };
+		CtrlService.device1OFF();
+    	};
 
 	// device 2
 	vm.device2ON = function () {
-		RESTservice.device2ON().then(function (response) {
-		});
-    };
+		CtrlService.device2ON();
+	};
 	vm.device2OFF = function () {
-		RESTservice.device2OFF().then(function (response) {
-		});
-    };
+		CtrlService.device2OFF();
+    	};
 
 	// device 3
 	vm.device3ON = function () {
-		RESTservice.device3ON().then(function (response) {
-		});
-    };
+		CtrlService.device3ON();
+    	};
 	vm.device3OFF = function () {
-		RESTservice.device3OFF().then(function (response) {
-		});
-    };
+		CtrlService.device3OFF();
+    	};
 
 	// device 4
 	vm.device4ON = function () {
-		RESTservice.device4ON().then(function (response) {
-		});
-    };
+		CtrlService.device4ON();
+    	};
 	vm.device4OFF = function () {
-		RESTservice.device4OFF().then(function (response) {
-		});
-    };
+		CtrlService.device4OFF();
+    	};
         
 	// device 5
 	vm.device5ON = function () {
-		RESTservice.device5ON().then(function (response) {
-		});
-    };
+		CtrlService.device5ON();
+    	};
 	vm.device5OFF = function () {
-		RESTservice.device5OFF().then(function (response) {
-		});
-    };
+		CtrlService.device5OFF();
+    	};
 
 	// device 6
 	vm.device6ON = function () {
-		RESTservice.device6ON().then(function (response) {
-		});
-    };
+		CtrlService.device6ON();
+    	};
 	vm.device6OFF = function () {
-		RESTservice.device6OFF().then(function (response) {
-		});
-    };
+		CtrlService.device6OFF();
+    	};
 		
 	$scope.vm = vm;
 	
 }]);
 
-// http service module
-device.service("RESTservice", ["$http", function ($http) {
+// http control service module
+device.service("CtrlService", ["$http", function ($http) {
 	// match with REST API endpoint in the backend
 	var url = "/device";
-return {
-		getItems: function () {
-		return $http.get(url);   
-		},
+	return {
 		
-	// Device Control Service
-
 		// device 1
 		device1ON: function () {
-		return $http.post(url + "/d1"); //ON   
+		return $http.post(url + "/d1");
 		},
 		device1OFF: function () {
-		return $http.get(url + "/d1"); //OFF   
+		return $http.get(url + "/d1");   
 		},
 
 		// device 2
